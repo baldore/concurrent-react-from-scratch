d3d366beb55ec463eee8a6aae8836829640c3cc1
baldore <acornssn@gmail.com>
2020-12-15 17:34:54 -0500

    Add babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
         singleQuote: true,
       },
     ],
+    'no-undef': 'error',
   },
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hola mundo genial')
+import { render, createTextElement, createElement } from './utils'
+
+// const textEl = createTextElement('hola mundo genial')
+// const element = createElement('h1', undefined, textEl)
+const element = <h1>hola mundo genial</h1>
+
+const container = document.getElementById('root')
+render(element, container)
